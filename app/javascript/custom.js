db09ba8ed5c8e79596300fa1b8d1d878ea94c61e
Phillip-Phan <phillipphan225@gmail.com>
2021-06-18 02:00:58 +1000

    aded back applicaiton.js

--- app/javascript/custom.js
+++
@@ -1,7 +0,0 @@
-let slider = document.getElementById("customRange");
-let label = document.getElementById("label");
-console.log("test");
-slider.oninput = function() {
-    label.innerHTML = slider.value;
-    console.log(slider.value);
-}
